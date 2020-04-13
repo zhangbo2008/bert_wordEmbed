93a7abb3a12a84d79d1db8a8e2f9c9db292ef5f1
zhangbo2008 <15122306087@163.com>
2020-04-13 14:23:16 +0800

    1

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ bert 词向量:
 
 https://blog.csdn.net/zhonglongshen/article/details/88125958
 
+https://storage.googleapis.com/bert_models/2018_11_03/chinese_L-12_H-768_A-12.zip
+权重下载完解压缩到e盘根目录即可.
 
 bert-serving-start  -model_dir E:/chinese_L-12_H-768_A-12 -num_worker=2bert-serving-start  -model_dir E:/chinese_L-12_H-768_A-12 -num_worker=2
 
